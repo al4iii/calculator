1a464856661dbb8a8de46f1c177f2a9ae335d982
Aliaksandr Danilin <al4icrux@gmail.com>
2020-11-16 16:30:25 +0300

    fix : after review

--- script.js
+++ script.js
@@ -1,122 +1,114 @@
-let numbers = document.querySelectorAll('.number');
-let operations = document.querySelectorAll('.operator');
-let decimalBtn = document.getElementById('decimal');
-let clearBtns = document.querySelectorAll('.clear-btn');   
-let resultBtn = document.getElementById('result');
-let piBtn = document.getElementById('pi');
-let display =document.getElementById('display');
-let MemoryCurrentNumber = 0; // what number is currently entered on the scoreboard, by default 0
-let MemoryNewNumber = false; // have we introduced a new meaning?
-let MemoryPendingOperation= '';    // pending operation
+  let numbers = document.querySelectorAll('.number');
+  let operations = document.querySelectorAll('.operator');
+  let decimalBtn = document.getElementById('decimal');
+  let clearBtns = document.querySelectorAll('.clear-btn');   
+  let resultBtn = document.getElementById('result');
+  let piBtn = document.getElementById('pi');
+  let display =document.getElementById('display');
+  let memoryCurrentNumber = 0; // what number is currently entered on the scoreboard, by default 0
+  let memoryNewNumber = false; // have we introduced a new meaning?
+  let memoryPendingOperation= '';    // pending operation
 
     
-for(let i=0; i<numbers.length; i++){
-  let number = numbers[i];
-  number.addEventListener('click', function(e){ //event handler
-    numberPress(e.target.textContent)
-  });
-};
-
-for(let i=0; i<operations.length; i++){
-  let operationBtn = operations[i];
-  operationBtn.addEventListener('click', function(e){    
-    operation(e.target.textContent);    
-  });
-};
-
-for(let i=0; i<clearBtns.length; i++){
-  let clearBtn = clearBtns[i];
-  clearBtn.addEventListener('click', function(e){    
-    clear(e.target.id);   
-  });
-}; 
-
-decimalBtn.addEventListener('click', decimal);
-piBtn.addEventListener('click', pi);
-
-function numberPress(number){
-  if(MemoryNewNumber){
-    display.value = number;
-    MemoryNewNumber = false;
-  }else{
-  if(display.value === '0'){
-    display.value = number;
-  }else{
-    display.value += number;
+  for(let i=0; i<numbers.length; i++){
+    let number = numbers[i];
+    number.addEventListener('click', function(e){ //event handler
+      numberPress(e.target.textContent)
+    });
   };
-};  
-};
 
-function operation(op){
-    let localOperationMemory = display.value;
-    if(MemoryNewNumber && MemoryPendingOperation !== "=") {
-        display.value = MemoryCurrentNumber;
-    } else {
-        MemoryNewNumber = true;
-        switch (MemoryPendingOperation) {
-            case "+":
-                MemoryCurrentNumber = MemoryCurrentNumber + Number(localOperationMemory);
-                break;
-            case "-":
-                MemoryCurrentNumber = MemoryCurrentNumber -  Number(localOperationMemory);
-                break;
-            case "*":
-                MemoryCurrentNumber = MemoryCurrentNumber * Number(localOperationMemory);
-                break;
-            case "/":
-                MemoryCurrentNumber = MemoryCurrentNumber / Number(localOperationMemory);
-                break;
-            case "x²":
-                MemoryCurrentNumber = display.value;            
-                MemoryCurrentNumber = Number(MemoryCurrentNumber) * Number(MemoryCurrentNumber);                     
-                MemoryPendingOperation = op;
-                break;
-            case "x³":
-                MemoryCurrentNumber = display.value;            
-                MemoryCurrentNumber = Number(MemoryCurrentNumber) * Number(MemoryCurrentNumber)* Number(MemoryCurrentNumber);
-                MemoryPendingOperation = op;
-                break;
-            case "√":
-                MemoryCurrentNumber = display.value;              
-                MemoryCurrentNumber = Math.sqrt(localOperationMemory);
-                MemoryPendingOperation = op;
-                break;
-            case "1/x":
-                MemoryCurrentNumber = display.value;               
-                MemoryCurrentNumber = 1/localOperationMemory;
-                MemoryPendingOperation = op;
-                break;
-            default:
-                MemoryCurrentNumber = +localOperationMemory; 
-                break;
-          };
-    display.value = MemoryCurrentNumber;
-    MemoryPendingOperation = op;
-};
-};
+  for(let i=0; i<operations.length; i++){
+    let operationBtn = operations[i];
+    operationBtn.addEventListener('click', function(e){    
+      operation(e.target.textContent);    
+    });
+  };
 
-function decimal(){
-  let localDecimalMemory = display.value;
-if(MemoryNewNumber){
-  localDecimalMemory = '0.';
-  MemoryNewNumber= false;
-}else{
-    if(localDecimalMemory.indexOf('.') === -1){
-      localDecimalMemory +='.'
+  for(let i=0; i<clearBtns.length; i++){
+    let clearBtn = clearBtns[i];
+    clearBtn.addEventListener('click', function(e){    
+      clear(e.target.id);   
+    });
+  }; 
+
+  decimalBtn.addEventListener('click', decimal);
+  piBtn.addEventListener('click', pi);
+
+
+  function numberPress(number){
+    if(memoryNewNumber){
+      display.value = number;
+      memoryNewNumber = false;
+    }else{
+    display.value === '0' ? display.value = number : display.value = display.value + number;
+    };
+  };  
+
+  function operation(op){
+      let localOperationMemory = display.value;
+      if(memoryNewNumber && memoryPendingOperation !== "=") {
+          display.value = memoryCurrentNumber;
+      } else {
+          memoryNewNumber = true;
+          switch (memoryPendingOperation) {
+              case "+":
+                  memoryCurrentNumber = memoryCurrentNumber + Number(localOperationMemory);
+                  break;
+              case "-":
+                  memoryCurrentNumber = memoryCurrentNumber -  Number(localOperationMemory);
+                  break;
+              case "*":
+                  memoryCurrentNumber = memoryCurrentNumber * Number(localOperationMemory);
+                  break;
+              case "/":
+                  memoryCurrentNumber = memoryCurrentNumber / Number(localOperationMemory);
+                  break;
+              case "x²":
+                  memoryCurrentNumber = display.value;            
+                  memoryCurrentNumber = Number(memoryCurrentNumber) * Number(memoryCurrentNumber);         
+                  break;
+              case "x³":
+                  memoryCurrentNumber = display.value;            
+                  memoryCurrentNumber = Number(memoryCurrentNumber) * Number(memoryCurrentNumber)* Number(memoryCurrentNumber);                
+                  break;
+              case "√":
+                  memoryCurrentNumber = display.value;              
+                  memoryCurrentNumber = Math.sqrt(localOperationMemory);                
+                  break;
+              case "1/x":
+                  memoryCurrentNumber = display.value;               
+                  memoryCurrentNumber = 1/localOperationMemory;                
+                  break;
+              default:
+                  memoryCurrentNumber = +localOperationMemory; 
+                  break;
+            };
+      display.value = memoryCurrentNumber;
+      memoryPendingOperation = op;
+  };
+  };
+
+  function decimal(){
+    let localDecimalMemory = display.value;
+  if(memoryNewNumber){
+    localDecimalMemory = '0.';
+    memoryNewNumber= false;
+  }else{
+      if(localDecimalMemory.indexOf('.') === -1){
+        localDecimalMemory +='.'
+      }    
+    }
+    display.value = localDecimalMemory;  
+  };
+
+  function clear(id){
+    if(id === 'ce'){
+      display.value = '0';
+      memoryNewNumber = true;
+    }else if(id === 'c'){
+      display.value = '0';
+      memoryNewNumber = true;
+      memoryCurrentNumber = 0;
+      memoryPendigOperation = '';    
     }    
-  }
-  display.value = localDecimalMemory;  
-};
-
-function clear(id){
-  if(id === 'ce'){
-    display.value = '0';
-    MemoryNewNumber = true;
-  }else if(id === 'c'){
-    display.value = '0';
-    MemoryNewNumber = true;
-    MemoryCurrentNumber = 0;
-    MemoryPendigOperation = '';    
-  }    
-};
- 
+  };
