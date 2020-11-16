77f2f16f385ef5c03b3f75bb8a6a82e4b6c93df8
Aliaksandr Danilin <al4icrux@gmail.com>
2020-11-16 12:32:51 +0300

    Revert "fix: after review"
    This reverts commit 994be1e4ad7156f831da8b400afba6f59dc6146f.
    
    # Conflicts:
    #	index.html
    #	script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var numbers = document.querySelectorAll(".number"),
     operations = document.querySelectorAll(".operator"),
     clearBtns = document.querySelectorAll(".clear-btn"),
@@ -9,6 +10,25 @@ var numbers = document.querySelectorAll(".number"),
     MemoryPendingOperation = "";
 
 
+=======
+let numbers = document.querySelectorAll('.number'),
+    operations = document.querySelectorAll('.operator'),
+    decimalBtn = document.getElementById('decimal'),
+    clearBtns = document.querySelectorAll('.clear-btn'),    
+    resultBtn = document.getElementById('result'),
+    piBtn = document.getElementById('pi'),
+    display =document.getElementById('display'),
+    MemoryCurrentNumber = 0, // какой номер сейчас введен на табло, по умолчанию 0
+    MemoryNewNumber = false, // ввели мы новое значение?
+    MemoryPendingOperation= '';    // операция в ожидании
+    
+for(let i=0; i<numbers.length; i++){
+  let number = numbers[i];
+  number.addEventListener('click', function(e){ //обработчик событий
+    numberPress(e.target.textContent)
+  });
+};
+>>>>>>> parent of 994be1e... fix: after review
 
 for(var i = 0; i < numbers.length; i++) {
     var number = numbers[i];
@@ -33,9 +53,49 @@ for(var i = 0; i < clearBtns.length; i++) {
 
 decimalBtn.addEventListener("click", decimal);
 
+<<<<<<< HEAD
 result.addEventListener("click", function(e){
     console.log("Клик по result")
 });
+=======
+function operation(op){
+  let localOperationMemory = display.value; 
+    if(MemoryNewNumber && MemoryPendingOperation !== "=") {
+      display.value = MemoryCurrentNumber;      
+    } else {
+      MemoryNewNumber = true;
+      if (MemoryPendingOperation === "+") {
+        MemoryCurrentNumber += +localOperationMemory;  
+      } else if (MemoryPendingOperation === "-") {
+        MemoryCurrentNumber -= +localOperationMemory;  
+      } else if (MemoryPendingOperation === "*") {
+        MemoryCurrentNumber *= +localOperationMemory;  
+      } else if (MemoryPendingOperation === "/") {
+        MemoryCurrentNumber /= +localOperationMemory;  
+      }else if (op === "x²") {        
+        MemoryCurrentNumber = display.value;            
+        MemoryCurrentNumber *= +MemoryCurrentNumber ;                     
+        MemoryPendingOperation = op;
+      }else if (op === "x³") {   
+        MemoryCurrentNumber = display.value;            
+        MemoryCurrentNumber *= +MemoryCurrentNumber * +MemoryCurrentNumber;
+        MemoryPendingOperation = op;      
+      }else if (op === "√") { 
+        MemoryCurrentNumber = display.value;              
+        MemoryCurrentNumber = Math.sqrt(localOperationMemory);
+        MemoryPendingOperation = op;                     
+      }else if (op === "1/x") {
+        MemoryCurrentNumber = display.value;               
+        MemoryCurrentNumber = 1/localOperationMemory;
+        MemoryPendingOperation = op; 
+      }else {
+        MemoryCurrentNumber = +localOperationMemory;  
+      }
+      display.value = MemoryCurrentNumber;
+      MemoryPendingOperation = op;
+    };
+};
+>>>>>>> parent of 994be1e... fix: after review
 
     function numberPress(number) {
         if(MemoryNewNumber) {
@@ -101,3 +161,18 @@ function operationPress(op) {
         }
     };
 
+<<<<<<< HEAD
+=======
+function clear(id){
+  if(id === 'ce'){
+    display.value = '0';
+    MemoryNewNumber = true
+  }else if(id === 'c'){
+    display.value = '0';
+    MemoryNewNumber = true;
+    MemoryCurrentNumber = 0;
+    MemoryPendigOperation = '';    
+  }  
+  console.log(MemoryNewNumber);
+};
+>>>>>>> parent of 994be1e... fix: after review
